05e0ce8e4f152af1bef7ed2663a1fc08dc299baa
Maxim Esin <mesin@micromine.com.au>
2023-05-14 00:18:04 +0800

    1

--- App/authConfig.js
+++ App/authConfig.js
@@ -8,7 +8,7 @@
 
 const msalConfig = {
     auth: {
-      clientId: "4440809a-62db-4edd-805e-38032cad78fc", // This is the ONLY mandatory field; everything else is optional.
+      clientId: "abbc495d-3b0e-4659-9520-ea7c40630fd1", // This is the ONLY mandatory field; everything else is optional.
       authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
       knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
       redirectUri: "https://esinmy.github.io/euB2C/App/", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
