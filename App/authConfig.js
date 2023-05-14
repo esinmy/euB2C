237fe581e43a8da9f7de098b45f6612eb397b2ea
esinmy <61378320+esinmy@users.noreply.github.com>
2023-05-14 22:06:36 +0800

    Update authConfig.js

--- App/authConfig.js
+++ App/authConfig.js
@@ -11,7 +11,7 @@ const msalConfig = {
       clientId: "abbc495d-3b0e-4659-9520-ea7c40630fd1", // This is the ONLY mandatory field; everything else is optional.
       authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
       knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
-      redirectUri: "https://esinmy.github.io/aulocalB2C/App/", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
+      redirectUri: "https://esinmy.github.io/euB2C/App/", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
     },
     cache: {
       cacheLocation: "sessionStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
