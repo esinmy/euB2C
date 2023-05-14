7e2f29ae2a00015f1a260f7eeb9819521d340954
esinmy <61378320+esinmy@users.noreply.github.com>
2023-05-14 22:13:14 +0800

    Update authConfig.js

--- App/authConfig.js
+++ App/authConfig.js
@@ -8,7 +8,7 @@
 
 const msalConfig = {
     auth: {
-      clientId: "abbc495d-3b0e-4659-9520-ea7c40630fd1", // This is the ONLY mandatory field; everything else is optional.
+      clientId: "6989d341-cdd9-4d57-a381-fb3923e9fc06", // This is the ONLY mandatory field; everything else is optional.
       authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
       knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
       redirectUri: "https://esinmy.github.io/euB2C/App/", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
